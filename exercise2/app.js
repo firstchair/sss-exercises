42f9751d640321a2ed8708e35c06efb776447dba
Lukas Feitsma <lukasfeitsma@xs4all.nl>
2014-11-27 15:29:19 +0100

    Task 3

--- exercise2/app.js
+++ exercise2/app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var task2Router = require('./routes/task2');
+app.use('/task2', task2Router);
+
+var task3 = require('./routes/task3');
+app.use('/task3', task3);
+
 // Setup serving static assets
 app.use(express.static('public'));
 
@@ -12,16 +18,23 @@ app.set('view engine', 'ejs');
 // Setup initial routing - index
 app.get('/', function(req, res) {
   res.render('index', {title: 'Server-side Scripting - Exercise 2'});
-});nodno
+});
 
 // Start the server
 var server = app.listen(3000, function(){
   console.log('App listening at http://localhost:3000');
 });
 
-var task2 = require('./routes/task2');
-app.get('/task2', task2);
+// var task2 = require('./routes/task2');
+// app.get('/task2', task2);
 
-module.exports = function(req, res){
-  res.send('Task 2');
-};
+// module.exports = function(req, res){
+//   res.send('Task 2');
+// };
+
+// // var task2 = function(req, res){
+// //   res.send('Task 2');
+// // };
+
+// module.exports = task2;
+
--- exercise2/routes/task2.js
+++ exercise2/routes/task2.js
@@ -1,3 +1,18 @@
-module.exports = function(req, res){
-  res.send('Task 2');
-};
+// module.exports = function(req, res){
+//   res.send('Task 2');
+// };
+
+
+// routes/task2.js
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.send('This is task2')
+});
+
+router.get('/sub-route', function(req, res){
+  res.send('This is a sub-route of task2, and is served @localhost:3000/task2/sub-route');
+});
+
+module.exports = router;
